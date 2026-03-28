e7feeff2d484d335f69814e2db17e1336412344d
Jason Lommelen <jason.lommelen@student.pxl.be>
2026-03-28 10:40:36 +0100

    Add VaultRareTech column to Settlements
    Introduce a new migration to add the VaultRareTech column to the Settlements table. This column is of type int, non-nullable, and has a default value of 0. Implement the Down method to allow migration reversal. Update the Designer file with auto-generated code reflecting these changes.

---
+++ Backend/TheFallenWastes_Infrastructure/Migrations/20260328092944_AddVaultRareTech.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheFallenWastes_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVaultRareTech : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VaultRareTech",
+                table: "Settlements",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VaultRareTech",
+                table: "Settlements");
+        }
+    }
+}
